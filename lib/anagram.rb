6abb66902a8cebfc42ef5f720c20e12b336081af
Angela <angelalamluu@gmail.com>
2020-11-23 23:42:02 -0800

    commit tree 63ca524d63f87df212ddcbe7ed0be3280a7e06d2

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+      @word = word
+    end
+  
+    def match(array)
+      array.select do |words|
+        is_anagram?(words)
+      end
+    end
+  
+    def is_anagram?(words)
+      words.chars.sort == @word.chars.sort
+    end
+  end
